1f9f609bd0b28a6babd002d8e2ed172ae9ea8ad0
Maria Filatkina <mfilatkina97@gmail.com>
2022-06-25 18:39:19 +0200

    add error handling

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,15 @@
+const ApiError = require("../error/ApiError");
+
 class userController {
   async registration(req, res) {}
 
   async login(req, res) {}
 
-  async check(req, res) {
+  async check(req, res, next) {
     const { id } = req.query;
+    if (!id) {
+      return next(ApiError.badRequest("ID not set"));
+    }
     res.json(id);
   }
 }
