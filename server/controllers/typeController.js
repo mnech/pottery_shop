7dda59a99055d830b34aa928805bbd40b499f799
Maria Filatkina <mfilatkina97@gmail.com>
2022-08-07 19:42:17 +0300

    add type controller

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -1,9 +1,30 @@
+const { TypeProduct } = require("../models/models");
+const ApiError = require("../error/ApiError");
+
 class typeController {
-  async create(req, res) {}
+  async create(req, res) {
+    const { name } = req.body;
+    const type = await TypeProduct.create({ name });
+    return res.json(type);
+  }
 
-  async getAll(req, res) {}
+  async getAll(req, res) {
+    const types = await TypeProduct.findAll();
+    return res.json(types);
+  }
 
-  async delete(req, res) {}
+  async delete(req, res) {
+    const { id } = req.query;
+    const result = await TypeProduct.destroy({
+      where: { id },
+    });
+
+    if (!result) {
+      return next(ApiError.badRequest("No type!"));
+    }
+
+    return res.json({ message: "Success" });
+  }
 }
 
 module.exports = new typeController();
