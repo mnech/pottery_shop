f657d78318ea4886486f99a0455a3ff32100a4ad
Maria Filatkina <mfilatkina97@gmail.com>
2022-08-09 18:26:48 +0300

    add func get and delete to product controller

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -26,11 +26,48 @@ class productController {
     }
   }
 
-  async getAll(req, res) {}
+  async getAll(req, res) {
+    let { typeProductId, limit, page } = req.query;
+    limit = limit || 10;
+    page = page || 1;
+    let offset = page * limit - limit;
 
-  async getOne(req, res) {}
+    let products;
 
-  async delete(req, res) {}
+    if (!typeProductId) {
+      products = await Product.findAndCountAll({ limit, offset });
+    } else {
+      products = await Product.findAndCountAll({
+        where: { typeProductId },
+        limit,
+        offset,
+      });
+    }
+
+    return res.json(products);
+  }
+
+  async getOne(req, res) {
+    const { id } = req.params;
+    const product = await Product.findOne({
+      where: { id },
+    });
+
+    return res.json(product);
+  }
+
+  async delete(req, res) {
+    const { id } = req.query;
+    const result = await Product.destroy({
+      where: { id },
+    });
+
+    if (!result) {
+      return next(ApiError.badRequest("No product!"));
+    }
+
+    return res.json({ message: "Success" });
+  }
 }
 
 module.exports = new productController();
